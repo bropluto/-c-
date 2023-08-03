cc0290be7e31662dd56fbdb52b20f574765b6c85
Pluto <135139067+bropluto@users.noreply.github.com>
2023-08-04 07:51:06 +0800

    Add files via upload

---
+++ test_5_27/test-5-27/test.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+//#include <string.h>
+/*int MAX = (int x, int y)
+{
+	if (a > b)
+		return x;
+	else(a < b)
+		return y;
+}
+int main()
+{
+	//printf("%d\n",strlen("我的第一个c程序"));
+	int a = 20;
+	int b = 30;
+	int MAX = 0;
+	max = MAX(a, b);
+	printf("%d\n", max);
+	//if (a > b)
+		//printf("最大值是：%d\n", a);
+	//else
+		//printf("最大值是：%d\n", b);
+
+	return 0;
+}
+*/
+//int main()
+//{
+//	int a = 10;
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+//	int gs = 0;
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof a);
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(arr) / sizeof(arr[4]));
+//	return 0;
+//int main()
+//{
+//	int a = 0;
+//	int b = ~a;
+//	printf("%d\n", b);
+//	return 0;
+/*int main（）{
+
+	int a = 90;
+	printf("%d\n", 'a');
+	return 0；
+}*/
+//int main()
+//{
+//	int a = 10;
+//	int b = a--;
+//	printf("a=%d b=%d\n", a, b);
+//	//printf("hallowrod\n");
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
